707bddd77c4cde9aab3a064fdb6abd4d8d939e63
Tanmay Jain <tanmayjainsre1975@gmail.com>
2025-09-24 10:32:24 +0530

    js

---
+++ JS/index.js
@@ -0,0 +1,91 @@
+// console.log("Hello World")
+// let a = 23;
+// console.log("a="+a);
+
+// let a =40;
+// if(a>10){
+//     let a =300;
+//     console.log("a inside"+a)
+// }
+// else{
+//     console.log("else condition");
+// }
+// console.log("a outiside"+a);
+
+//* == works on value;
+//* === works on type as well;
+
+// let a='20';
+// if(a===20){
+//     console.log("Equal Data");
+// }
+// else{
+//     console.log("Not equal")
+// }
+
+// const myname="rahul";
+// console.log("Hi...."+myname);
+// const data = `hello....${myname}`;
+// console.log(data);
+
+// function msg(){
+//     return "welcome to JS";
+// }
+
+// const data = msg();
+// console.log(data);
+
+// const data = (name){
+//     return name+" welcome to JS";
+// }
+
+// const msg = data("Rahul");
+// console.log(msg);
+
+
+// const data =(name)=>{
+//     // console.log("hi....hello"+name);
+//     return "hii......helllo"+name;
+// }
+// const result = data("rahul");
+// data("Rahul");
+
+// const data = msg =>msg;
+// console.log(data("greeting"))
+
+// (()=>{
+//     console.log("Hello World")
+// })();
+
+// setTimeout(()=>{
+//     console.log("hii.. i m using Timeout");
+// },1000);
+
+// setInterval(()=>{
+//     console.log("hooray... i m using Interval");
+// },1000);
+
+// function greetings(msg="Welcome"){
+//     console.log("hiiii"+" "+msg);
+// }
+// greetings();
+
+function selectlanguage(lang){
+    let data;
+    if(lang=='java'){
+        function javaCompiler(){
+            return "java compile selectd";
+    }
+    data = javaCompiler();
+    }
+    elseif(lang == 'c'){
+        function cCompiler(){
+            return "c compile selected";
+        }
+        data = cCompiler();
+    }
+    else{
+        data = "no such compiler";
+        }
+        console.log(data)
+}
